5796aad30f7cc0d3980f9f74b28989002a13af01
dimma111 <marinenko96@yandex.ru>
2022-03-04 19:46:44 +0500

    add provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../app/store/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <div className="py-6 px-1">
+        <Component {...pageProps} />
+      </div>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
