6ce5b810b7acea78ee33fc1f770b6f94edaca789
dimma111 <marinenko96@yandex.ru>
2022-03-07 14:42:35 +0500

    Create cart actions

---
+++ app/hooks/useActions.ts
@@ -0,0 +1,13 @@
+import { cartActions } from "./../store/cart/cart.slice";
+import { bindActionCreators } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+
+const allActions = {
+  ...cartActions,
+};
+
+export const useActions = () => {
+  const dispatch = useDispatch();
+
+  return bindActionCreators(allActions, dispatch);
+};
