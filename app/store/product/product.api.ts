2c0b97b82a80970575bec0c462ea3687f1cc7a22
dimma111 <marinenko96@yandex.ru>
2022-03-07 14:45:12 +0500

    Edit link api

--- app/store/product/product.api.ts
+++ app/store/product/product.api.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { IProduct } from "./products.types";
+import { IProduct } from "./product.types";
 
 export const productApi = createApi({
   reducerPath: "api/products",
